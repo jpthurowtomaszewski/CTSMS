33ffde6b0e355427d843df018bff21478f6d7269
João Pedro Thurow Tomaszewski <JPthurowtomaszewski@hotmail.com>
2022-05-12 15:24:58 -0300

    Cabeçalho e Titulo da tela Gestao

--- lib/pages/home_gestores.dart
+++ lib/pages/home_gestores.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class HomeGestores extends StatelessWidget {
@@ -6,8 +10,28 @@ class HomeGestores extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('CTSMS',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        actions: const [
+          IconButton(
+            padding: EdgeInsets.all(18),
+            icon: Icon(Icons.login),
+            tooltip: 'Sair',
+            onPressed: null,
+          ),
+        ],
+      ),
       body: Center(
-        child: Text('Botões'),
+        child: Container(
+          padding: EdgeInsets.all(18),
+          child: const Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Gestão',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              )),
+        ),
       ),
     );
   }
---
+++ lib/pages/veiculos_lista.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VeiculosLista extends StatefulWidget {
+  VeiculosLista({Key? key}) : super(key: key);
+
+  @override
+  State<VeiculosLista> createState() => _VeiculosListaState();
+}
+
+class _VeiculosListaState extends State<VeiculosLista> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
