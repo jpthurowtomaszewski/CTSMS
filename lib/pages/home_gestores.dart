8addcb81d20a4ebf587ef69f01abbaabf9c3de10
João Pedro Thurow Tomaszewski <JPthurowtomaszewski@hotmail.com>
2022-05-12 17:41:32 -0300

    Tela Gestores com botoes e tela listagemVeiculos

--- lib/pages/home_gestores.dart
+++ lib/pages/home_gestores.dart
@@ -1,38 +1,127 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 //import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:tcc/pages/veiculos_lista.dart';
 
-class HomeGestores extends StatelessWidget {
-  const HomeGestores({Key? key}) : super(key: key);
+class HomeGestores extends StatefulWidget {
+  HomeGestores({Key? key}) : super(key: key);
+  @override
+  State<HomeGestores> createState() => _HomeGestoresState();
+}
+
+class _HomeGestoresState extends State<HomeGestores> {
+  veiculosLista() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => VeiculosLista(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('CTSMS',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-        actions: const [
-          IconButton(
-            padding: EdgeInsets.all(18),
-            icon: Icon(Icons.login),
-            tooltip: 'Sair',
-            onPressed: null,
-          ),
-        ],
-      ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(18),
-          child: const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Gestão',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              )),
+        appBar: AppBar(
+          title: const Text('CTSMS',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          actions: const [
+            IconButton(
+              padding: EdgeInsets.all(18),
+              icon: Icon(Icons.login),
+              tooltip: 'Sair',
+              onPressed: null,
+            ),
+          ],
         ),
-      ),
-    );
+        body: Center(
+          child: Container(
+              padding: EdgeInsets.all(18),
+              child: Column(
+                children: [
+                  const Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Gestão',
+                        style: TextStyle(
+                            fontSize: 28, fontWeight: FontWeight.bold),
+                      )),
+                  Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(top: 30, left: 24, right: 24),
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(backgroundColor:
+                              MaterialStateProperty.resolveWith<Color?>(
+                                  (states) {
+                            return Colors.black;
+                          })),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                  padding: EdgeInsets.all(16),
+                                  child: Text(
+                                    'Ver no Mapa',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ))
+                            ],
+                          ))),
+                  Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(top: 10, left: 24, right: 24),
+                      child: ElevatedButton(
+                          onPressed: () => veiculosLista(),
+                          style: ButtonStyle(backgroundColor:
+                              MaterialStateProperty.resolveWith<Color?>(
+                                  (states) {
+                            return Colors.black;
+                          })),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                  padding: EdgeInsets.all(16),
+                                  child: Text(
+                                    'Veículos',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ))
+                            ],
+                          ))),
+                  Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(top: 10, left: 24, right: 24),
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(backgroundColor:
+                              MaterialStateProperty.resolveWith<Color?>(
+                                  (states) {
+                            return Colors.black;
+                          })),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                  padding: EdgeInsets.all(16),
+                                  child: Text(
+                                    'Motoristas',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ))
+                            ],
+                          ))),
+                ],
+              )),
+        ));
   }
 }
--- lib/pages/veiculos_lista.dart
+++ lib/pages/veiculos_lista.dart
@@ -10,6 +10,30 @@ class VeiculosLista extends StatefulWidget {
 class _VeiculosListaState extends State<VeiculosLista> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CTSMS',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        actions: const [
+          IconButton(
+            padding: EdgeInsets.all(18),
+            icon: Icon(Icons.arrow_back),
+            tooltip: 'Voltar',
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(18),
+          child: const Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Veículos',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              )),
+        ),
+      ),
+    );
   }
 }
